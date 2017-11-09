95747018405779ee6d41c5c7ba55e975af9055dc
nityanarayan44 <nityanarayan44@live.com>
2017-11-09 14:46:11 +0530

    Codebase has been modified

--- ChromeExtensions/XpathFinder/FindXPath/background.js
+++ ChromeExtensions/XpathFinder/FindXPath/background.js
@@ -9,31 +9,47 @@
 //================================================================================================================
 // Global vars
 //================================================================================================================
-var recStatus 	= 'OFF';
+var recStatus = 'OFF';
 var message 	= 'Not Recording !';
 var elementArray= [];
 
 // Angular Directives for the filter.
 // will be used to extract the property of an element.
-var angularDirectives = ["ng-bind","ng-click","ng-if","ng-model","ng-class","ng-href", "ng-submit", "ng-src", "ng-view","ng-show","ng-repeat","ng-checked","ng-change","ng-pattern","ng-required","ng-minlength","ng-maxlength"];
+var angularDirectives = [ "ng-bind",
+													"ng-click",
+													"ng-if",
+													"ng-model",
+													"ng-class",
+													"ng-href",
+													"ng-submit",
+													"ng-src",
+													"ng-view",
+													"ng-show",
+													"ng-repeat",
+													"ng-checked",
+													"ng-change",
+													"ng-pattern",
+													"ng-required",
+													"ng-minlength",
+													"ng-maxlength"
+												];
 
 // Properties
 // tagName, name, className, id, textContext, role, [with previous element]
 var data = {
-	"element"	: "",
-	"name"		: "",
-	"className"	: "",
-	"text"		: "",
-	"xpath"		: "",
-	"xpath_Suggestions"	: "",
-	"id"		: "",
-	"dimension"	: ""
+						"element"		: "",
+						"name"			: "",
+						"className"	: "",
+						"text"			: "",
+						"xpath"			: "",
+						"id"				: "",
+						"dimension"	: "",
+						"xpath_Suggestions"	: ""
 };
-
 
 //================================================================================================================
 // Extension Background Services
-// Messages passing [injection, popup]
+// Messages passing between [injection, popup]
 //================================================================================================================
 	chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 		console.log('Message Recieved...');
@@ -56,13 +72,13 @@ var data = {
 		if(msg.from == 'injection' && msg.event == 'on-record') {
 			//Recording the actions through the injected script
 			//console.dir(msg);
-			data.element= msg.data.element;
-			data.name 	= msg.data.name;
-			data.className=msg.data.className;
-			data.text 	= msg.data.text;
-			data.xpath 	= msg.data.xpath;
-			data.id		= msg.data.id;
+			data.element	= msg.data.element;
+			data.name 		= msg.data.name;
+			data.text 		= msg.data.text;
+			data.xpath 		= msg.data.xpath;
+			data.id				= msg.data.id;
 			data.dimension= msg.data.dimension;
+			data.className= msg.data.className;
 			// XPath suggestions based on the data object.
 			data.xpath_Suggestions = suggetionsXPath(data);
 
@@ -76,43 +92,49 @@ var data = {
 // Functions
 //================================================================================================================
 	function sendDataToPopup() {
-		console.log("Sending Data to Popup ");
-		chrome.runtime.sendMessage({
-			'from'		:	'background',
-			'event'		:	'on-popup-open-response',
-			'message'	: 	message,
-			'data'		: 	data
-		});
-	}
+						console.log("Sending Data to Popup ");
+						// Sending Msg from background.
+						// popup.js will recieve this message.
+						chrome.runtime.sendMessage({
+																				'from'		:	'background',
+																				'event'		:	'on-popup-open-response',
+																				'message'	: 	message,
+																				'data'		: 	data
+						});
+	}// End of sendDataToPopup
 
 	/*
 	 * Workings on XPath suggestion.
 	 **/
 	function suggetionsXPath(obj){
-		var sugg = "";
-		if(obj !== undefined && obj !== null ){
-			// Suggestions based on ID
-			sugg += ( obj.id !== "" && obj.id !== undefined) ? ("//*[@id='"+ obj.id +"'] <br/> //*[contains(@id,'"+ obj.id +"')] <br/>") : "" ;
+																// Suggestions variable, includes all the available suggestion for the current element based on its META.
+																var sugg = "";
 
-			// Suggestion based on Name
-			sugg += ( obj.name !== "" && obj.name !== undefined) ? (" //*[@name='"+ obj.name +"'] <br/> //*[contains(@name,'"+ obj.name +"')] <br/>") : "" ;
+																// processing the provided object.
+																if(obj !== undefined && obj !== null ){
+																																			// Suggestions based on ID
+																																			sugg += ( obj.id !== "" && obj.id !== undefined) ? ("//*[@id='"+ obj.id +"'] <br/> //*[contains(@id,'"+ obj.id +"')] <br/>") : "" ;
 
-			// Suggestion based on className
-			sugg += ( obj.className !== "" && obj.className !== undefined) ? (" //*[@class='"+ obj.className +"'] <br/> //*[contains(@class,'"+ obj.className +"')] <br/>") : "" ;
+																																			// Suggestion based on Name
+																																			sugg += ( obj.name !== "" && obj.name !== undefined) ? (" //*[@name='"+ obj.name +"'] <br/> //*[contains(@name,'"+ obj.name +"')] <br/>") : "" ;
 
-			// Suggestion based on innerText
-			sugg += ( obj.text !== "" && obj.text !== undefined) ? (" //*[text()='"+ obj.text +"'] <br/> //*[contains(text(),'"+ obj.text +"')] <br/>") : "" ;
+																																			// Suggestion based on className
+																																			sugg += ( obj.className !== "" && obj.className !== undefined) ? (" //*[@class='"+ obj.className +"'] <br/> //*[contains(@class,'"+ obj.className +"')] <br/>") : "" ;
 
-			// Suggestion based on ID and/or innerText
-			sugg += ( (obj.id !== undefined && obj.id !== "" && obj.id.length !== 0) && (obj.text !== undefined && obj.text !== "" && obj.text.length !== 0)) ? (" //*[@id='"+ obj.id +"' and text()='"+ obj.text +"'] <br/> //*[@id='"+ obj.id +"' and contains(text(),'"+ obj.text +"')] <br/>") : "";
+																																			// Suggestion based on innerText
+																																			sugg += ( obj.text !== "" && obj.text !== undefined) ? (" //*[text()='"+ obj.text +"'] <br/> //*[contains(text(),'"+ obj.text +"')] <br/>") : "" ;
 
-			// Suggestion based on Class and/or innerText
-			sugg += ( (obj.className !== undefined && obj.className !== "") && (obj.text !== undefined && obj.text !== "") ) ? (" //*[@class='"+ obj.className +"' and contains(text(), '"+ obj.text +"') ] <br/> //*[@class='"+ obj.className +"' and text()='"+ obj.text +"'] <br/>") : "" ;
+																																			// Suggestion based on ID and/or innerText
+																																			sugg += ( (obj.id !== undefined && obj.id !== "" && obj.id.length !== 0) && (obj.text !== undefined && obj.text !== "" && obj.text.length !== 0)) ? (" //*[@id='"+ obj.id +"' and text()='"+ obj.text +"'] <br/> //*[@id='"+ obj.id +"' and contains(text(),'"+ obj.text +"')] <br/>") : "";
 
-			// Suggestion based on dimension
-			sugg += ( obj.dimension !== "" && obj.dimension !== undefined) ? ("<br/> dimension (x, y) can be used in Action Class or Robot class of java.") : "";
-		}
-		if (sugg.length === 0) sugg += "Oopse, No Suggestions at this moment.";
+																																			// Suggestion based on Class and/or innerText
+																																			sugg += ( (obj.className !== undefined && obj.className !== "") && (obj.text !== undefined && obj.text !== "") ) ? (" //*[@class='"+ obj.className +"' and contains(text(), '"+ obj.text +"') ] <br/> //*[@class='"+ obj.className +"' and text()='"+ obj.text +"'] <br/>") : "" ;
 
-		return sugg;
-	}
+																																			// Suggestion based on dimension
+																																			sugg += ( obj.dimension !== "" && obj.dimension !== undefined) ? ("<br/> dimension (x, y) can be used in Action Class or Robot class of java.") : "";
+																}// End of if
+																if (sugg.length === 0) sugg += "Oopse, No Suggestions at this moment.";
+
+																// returning the final XPath Suggestions, after processing.
+																return sugg;
+	}// End of suggetionsXPath function.
