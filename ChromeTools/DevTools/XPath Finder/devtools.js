cdf7885dbce1dd89c320be51b39f95f6fe2dc874
nityanarayan44 <nityanarayan44@live.com>
2017-11-13 18:38:36 +0530

    DevTools file has been removed for tremporary

--- ChromeTools/DevTools/XPath Finder/devtools.js
+++
@@ -1,8 +0,0 @@
-// Create a new panel
-chrome.devtools.panels.create("XPath Finder",
-  "icon.png",
-  "panel.html",
-  function (panel) {
-      console.log("I was injected. Thats why i showed up");
-  }
-);
